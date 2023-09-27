47b827f78201fba2a685ed14710f44122b7c4972
H M Mohiuddin <mohiuddin3824@gmail.com>
2023-09-27 23:17:08 +0600

    Task 4 Updated!

--- task_4.php
+++ task_4.php
@@ -20,42 +20,28 @@ $studentGrades = [
         "Science" => 70,
     ]
     
-    
-
 ];
 
 
 function studentData($studentGrades){
     
-    echo $studentGrades["jamal"];
+    foreach($studentGrades as $student => $grades){
+        $average = array_sum($grades) / count($grades);
+        if ($average>=80) {
+            echo "The Grade of {$student} is A+ \n";
+            
+        }elseif($average>70 && $average<=79){
+            echo "The Grade of {$student} A \n";
+            
+        }elseif($average>=60 && $average<69){
+            echo "The Grade of {$student} B \n";
+            
+        }else{
+            echo "The Grade of {$student} F \n";
+            
+        }
+    }
     
 }
 
 echo studentData($studentGrades);
-
-/*
-$total_mark = $bangla + $english + $math;
-
-    $grade = $total_mark / 3;
-
-    if ($grade>=70) {
-        echo "<span class=\"grade\"> Your Grade <br> A </span>";
-        echo "<span class=\"point\"> Your Point <br> 4.00 </span>";
-        
-    }elseif($grade>=50 && $grade<59){
-        echo "<span class=\"grade\"> Your Grade <br> B </span>";
-        echo "<span class=\"point\"> Your Point <br> 3.00 </span>";
-        
-    }elseif($grade>=40 && $grade<49){
-        echo "<span class=\"grade\"> Your Grade <br> C </span>";
-        echo "<span class=\"point\"> Your Point <br> 2.00 </span>";
-        
-    }elseif($grade>=33 && $grade<39){
-        echo "<span class=\"grade\"> Your Grade <br> D </span>";
-        echo "<span class=\"point\"> Your Point <br> 1.00 </span>";
-        
-    }else{
-        echo "<span class=\"grade\"> Your Grade <br> F </span>";
-        echo "<span class=\"point\"> Your Point <br> 0.00 </span>";
-        
-    } */
